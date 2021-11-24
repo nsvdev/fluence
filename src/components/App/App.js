c1af8f624ea99f39727825b5a8ff2185f6c22185
vladko-89 <63239096+vladko-89@users.noreply.github.com>
2021-11-24 18:04:22 +0400

    feat: page-claming-9

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,11 @@ import { Routes, Route} from 'react-router-dom';
 import PageBegin from '../../pages/begin-page/begin-page';
 import FirstStepPage from '../../pages/step1-page/step1-page';
 import ProofPage from '../../pages/proof-page/proof-page';
+import DelegationPage from '../../pages/delegation-page/delegation-page';
+import DonePage from '../../pages/done-page/done-page';
+
 import ConnectWallet from '../ConnectWallet/ConnectWallet';
-import DelegationPage from '../../pages/delegation-page/delegation-page'
+
 
 import './App.css';
 
@@ -17,6 +20,7 @@ function App() {
         <Route path='/proof' element={<ProofPage/>} />
         <Route path='/delegation' element={<DelegationPage/>} />
         <Route path='/wallet' element={<FirstStepPage/>} />
+        <Route path='/done' element={<DonePage/>} />
         <Route path='/connect' element={<ConnectWallet />} />
         <Route path='/' element={<PageBegin/>} />
 
---
+++ src/pages/done-page/done-page.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Header from '../../components/Header/Header';
+import Progress from '../../components/Progress/Progress';
+import Title from '../../components/Title/Title';
+import WalletInfo from '../../components/WalletInfo/WalletInfo';
+import Button from '../../components/Button/Button';
+
+import styles from './done-page.module.css';
+
+const DonePage = () => {
+    return (
+        <div className={styles.background}>
+            <Header />
+            <div className="container">
+                <main className="main">
+                    <div className={styles.progress}>
+                        <Progress />
+                    </div>
+                    <ul className={styles.wallets}>
+                        <li className={styles.wallet}>
+                            <WalletInfo wallet="wallet" account="0x24343242..534" />
+                        </li>
+                        <li className={styles.wallet}>
+                            <WalletInfo wallet="delegate" account="0xD69B...1fE4" />
+                        </li>
+                    </ul>
+                    
+                    <div className={styles.title}>
+                        <Title type="large" text="Almost done! You can claim your reward now"  />
+                    </div>
+
+                    <p className={styles.caption}>
+                        0.002 ETH (~$30) gas fee will be involved for claiming transaction
+                    </p>
+                    
+                    <div className={styles.button}>
+                        <Button text='Claim 500 FLT' />
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
+}
+
+export default DonePage;
